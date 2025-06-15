22b9ec69e00dfeeac5cfc179361977ac2104b41c
Pasan-basitha <pasanbasitha9733@gmail.com>
2025-06-15 11:31:52 +0530

    add the yml file

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -2,14 +2,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 public class HomePageTest {
 
     @Test
     public void firstTest(){
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         //driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        ChromeOptions options = new ChromeOptions();
+
+
+
+        String seleniumUrl = System.getenv("SELENIUM_REMOTE_URL");
+        if (seleniumUrl == null || seleniumUrl.isEmpty()) {
+            // Default to localhost for local development
+            seleniumUrl = "http://localhost:4444/wd/hub";
+        }
+
+        URI uri = URI.create(seleniumUrl);
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(uri.toURL(), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
         driver.get("https://www.leafground.com/dashboard.xhtml");
 
         //enter value for Text input
